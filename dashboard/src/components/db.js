9439e32bbc7f7d19f36c4c4090e40413a4bb26a8
TheZoap <lucas.holstein@gmail.com>
2024-09-04 12:12:05 +0200

    New layout

--- dashboard/src/components/db.js
+++ dashboard/src/components/db.js
@@ -1,45 +1,30 @@
 import { db } from "../config/firebase";
-import { useEffect, useState} from "react";
+import { useEffect, useState, useMemo, useCallback } from "react";
 import {addDoc, getDocs, collection, deleteDoc, doc } from "firebase/firestore";
+import Box from '@mui/material/Box';
+import CorrectAnswersList from './answerComponents/CorrectAnswersList';
+import IncorrectAnswersList from './answerComponents/IncorrectAnswersList';
+import AnswerForm from './answerComponents/AnswerForm';
+import AnswerTotals from './answerComponents/AnswerTotals';
+import AnswersTable from './answerComponents/answerTable';
 
 export const Database = () => {
-
-    const [studentList, setStudentList] = useState([]);
-
-    const studentCollectionRef = collection(db,"students");
 
     const [answerList, setAnswerList] = useState([]);
     const [correctAnswers, setCorrectAnswers] = useState([]);
     const [incorrectAnswers, setIncorrectAnswers] = useState([]);
+    const [newAnswer, setAnswer] = useState("");
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
 
-    const answerCollectionRef = collection(db,"answers");
+    const answerCollectionRef = useMemo(() => collection(db,"answers"),[db]);
 
-    // useEffect(() => {
-    //     const getStudentList = async () => {
-    //         // READ DATA FROM DATABASE
-    //         // SET THE STUDENT LIST
-    //         try {
-    //             const data = await getDocs(studentCollectionRef);
-    //             const filteredData = data.docs.map((doc) =>({
-    //                 ...doc.data(),id:doc.id}))
-    //             console.log(filteredData);
-    //             setStudentList(filteredData);
-                
-    //         } catch (err) {
-    //             console.error(err);
-    //         }
-            
-    //     }
-    //     getStudentList();
-    // },[])
-    const getAnswerList = async () => {
-        // READ DATA FROM DATABASE
-        // SET THE ANSWER LIST
+    const getAnswerList = useCallback(async () => {
         try {
             const dataAnswers = await getDocs(answerCollectionRef);
-            const filteredDataAnswers = dataAnswers.docs.map((doc) =>({
-                ...doc.data(),id:doc.id}))
-            console.log(filteredDataAnswers);
+            const filteredDataAnswers = dataAnswers.docs.map((doc) => ({
+                ...doc.data(), id: doc.id
+            }));
+
             setAnswerList(filteredDataAnswers);
 
             // Separate answers into correct and incorrect
@@ -48,87 +33,91 @@ export const Database = () => {
 
             setCorrectAnswers(correctAnswer);
             setIncorrectAnswers(incorrectAnswer);
-            
+
         } catch (err) {
             console.error(err);
         }
-    }
-    getAnswerList();
-    useEffect(() => {
-        getAnswerList();
-    },[])
-
-    // New Answer
+    }, [answerCollectionRef]);
 
     const onSubmitAnswer = async () => {
         try {
-            await addDoc(answerCollectionRef,{
-                answerName:newAnswer,
-                correct:isAnswerCorrect
-            })
+            await addDoc(answerCollectionRef, {
+                answerName: newAnswer,
+                correct: isAnswerCorrect
+            });
 
             getAnswerList();
         } catch (err) {
             console.error(err);
         }
-    }
+    };
 
     const deleteAnswer = async (id) => {
-        const answerDoc = doc(db,"answers", id);
-        await deleteDoc(answerDoc);
-    }
+        try {
+            const answerDoc = doc(db, "answers", id);
+            await deleteDoc(answerDoc);
+            getAnswerList(); // Refresh the list after deletion
+        } catch (err) {
+            console.error(err);
+        }
+    };
 
-    const [newAnswer, setAnswer] = useState("");
-    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+    useEffect(() => {
+        getAnswerList();
+    }, [getAnswerList]);
 
-return (
-    <div>
-        <h1>Correct Answers</h1>
-        {correctAnswers.map((answer) => (
-            <div key={answer.id}>
-                <h2 style={{ color: "green" }}>{answer.answerName}</h2> <button onClick={() => deleteAnswer(answer.id)}>Delete</button>
-                <p style={{ color: "black" }}>
-                    Options: {Array.isArray(answer.testOptions) ? answer.testOptions.join(", ") : answer.testOptions}
-                </p>
-                <p style={{ color: "black" }}>
-                    Question Template: {answer.testQuestionTemplate?._key?.path?.segments?.slice(-1)[0]}
-                </p>
-            </div>
-        ))}
+    return (
+        <div>
+        <Box display="flex" flexDirection="row" gap={2}>
+            <Box flex={2}>
+                <AnswersTable
+                    correctAnswers={correctAnswers}
+                    incorrectAnswers={incorrectAnswers}
+                    onDelete={deleteAnswer}
+                />
+            </Box>
+            <Box flex={1} display = "flex" flexDirection="column" gap={2}>
+                <Box>
+                <AnswerForm 
+                    onSubmit={onSubmitAnswer} 
+                    newAnswer={newAnswer} 
+                    setAnswer={setAnswer} 
+                    isAnswerCorrect={isAnswerCorrect} 
+                    setIsAnswerCorrect={setIsAnswerCorrect} 
+                />
+                </Box>
+                <Box>
+                <AnswerTotals 
+                correctAnswers={correctAnswers} 
+                incorrectAnswers={incorrectAnswers} />
+                </Box>
+            </Box>
+        </Box>
         
-        <h1>Incorrect Answers</h1>
-        {incorrectAnswers.map((answer) => (
-            <div key={answer.id}>
-                <h2 style={{ color: "red" }}>{answer.answerName}</h2> <button onClick={() => deleteAnswer(answer.id)}>Delete</button>
-                <p style={{ color: "black" }}>
-                    Options: {Array.isArray(answer.testOptions) ? answer.testOptions.join(", ") : answer.testOptions}
-                </p>
-                <p style={{ color: "black" }}>
-                    Question Template: {answer.testQuestionTemplate?._key?.path?.segments?.slice(-1)[0]}
-                </p>
-
-            </div>
-        ))}
-
-        <h1>Totals</h1>
-        <h3 style={{ color: "green" }}>
-            Total correct answers: {correctAnswers.length}
-        </h3>
-        <h3 style={{ color: "red" }}>
-            Total incorrect answers: {incorrectAnswers.length}
-        </h3>
-        <h3 style={{ color: "black" }}>
-            Total percentage correct: {(correctAnswers.length/(incorrectAnswers.length + correctAnswers.length)* 100).toFixed(2) + '%'}
-        </h3>
-
-        <h1>Create new entry</h1>
-        <input placeholder = "Answer" 
-        onChange={(inputEvent) => setAnswer(inputEvent.target.value)}/>
-        <input type = "checkbox" 
-        checked={isAnswerCorrect}
-        onChange={(inputEvent) => setIsAnswerCorrect(inputEvent.target.checked)}/>
-        <label>Correct</label>
-        <button onClick={onSubmitAnswer}>Submit answer</button>
-    </div>
-);
-}
+        </div>
+    );
+    // return (
+    //     <div>
+    //         <div>
+    //         <AnswersTable
+    //             correctAnswers={correctAnswers}
+    //             incorrectAnswers={incorrectAnswers}
+    //             onDelete={deleteAnswer}
+    //         />
+    //         </div>
+            
+    //         {/* <CorrectAnswersList correctAnswers={correctAnswers} onDelete={deleteAnswer} /> */}
+    //         {/* <IncorrectAnswersList incorrectAnswers={incorrectAnswers} onDelete={deleteAnswer} /> */}
+    //         <AnswerTotals correctAnswers={correctAnswers} incorrectAnswers={incorrectAnswers} />
+    //         <div>
+    //             <AnswerForm 
+    //             onSubmit={onSubmitAnswer} 
+    //             newAnswer={newAnswer} 
+    //             setAnswer={setAnswer} 
+    //             isAnswerCorrect={isAnswerCorrect} 
+    //             setIsAnswerCorrect={setIsAnswerCorrect} 
+    //         /></div>
+            
+    //     </div>
+    // );
+};
